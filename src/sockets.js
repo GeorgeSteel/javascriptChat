5f73eccdb31955d24d5e1f2058cee6a3f89fb256
jorge <julianacero98@gmail.com>
2018-12-04 02:16:26 -0600

    se cambio el arreglo por un objeto JSON

--- src/sockets.js
+++ src/sockets.js
@@ -1,35 +1,56 @@
 module.exports = function (io){
-    let nicknames = [];
+    let users = {};
     io.on('connection', socket => {
         console.log('new user connected');
 
         socket.on('new user', (data, cb) => {
             console.log(data);
-            if(nicknames.indexOf(data) != -1){
+            if(data in users){
                 cb(false);
             }else{
                 cb(true);
                 socket.nickname = data;
-                nicknames.push(socket.nickname);
+                users[socket.nickname] = socket;
                 updateNicknames();
             }
         });
 
-        socket.on('send message', function (data){
-            io.sockets.emit('new message', {
-                msg: data,
-                nick: socket.nickname
-            });
+        socket.on('send message', (data, cb) => {
+            var msg = data.trim();
+
+            if (msg.substr(0,3) === '/p ') {
+                msg = msg.substr(3);
+                const index = msg.indexOf(' ');
+                if (index !== -1) {
+                    var name = msg.substr(0, index);
+                    var msg = msg.substr(index + 1);
+                    if (name in users) {
+                        users[name].emit('private', {
+                            msg,
+                            nick: socket.nickname
+                        });
+                    }else{
+                        cb('Error! Please enter a valid user');
+                    }
+                }else{
+                    cb('Error! Please enter your message');
+                }
+            } else {
+                io.sockets.emit('new message', {
+                    msg: data,
+                    nick: socket.nickname
+                });
+            }            
         });
         
         socket.on('disconnect', function (data){
             if (!socket.nickname) return;
-            nicknames.splice(nicknames.indexOf(socket.nickname), 1);
+            delete users[socket.nickname];
             updateNicknames();
         });
 
         function updateNicknames() {            
-            io.sockets.emit('usernames', nicknames);
+            io.sockets.emit('usernames', Object.keys(users));
         }
     });
 }
